1ea8f8546e60b564ee87adab80bb9c2ba01f9f48
yarastqt <yarastqt@yandex-team.ru>
2023-05-07 02:28:41 +0400

    chore: init repo

---
+++ .prettierrc.js
@@ -0,0 +1,19 @@
+const {builtinModules} = require('module');
+
+module.exports = {
+  arrowParens: 'always',
+  endOfLine: 'lf',
+  printWidth: 100,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  importOrder: [
+    `^(${builtinModules.join('|')})$`,
+    '<THIRD_PARTY_MODULES>',
+    '^@',
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
+};
---
+++ index.js
@@ -0,0 +1,6 @@
+import { AppRegistry } from 'react-native'
+
+import { Application } from './src/application'
+import { name } from './app.json'
+
+AppRegistry.registerComponent(name, () => Application)
