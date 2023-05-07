97eccee39366f9294c06a5ace9ae592ba1afc4d3
yarastqt <yarastqt@gmail.com>
2023-05-08 01:12:10 +0400

    feat: add action button for main screen

--- src/screens/main-screen/main-screen.tsx
+++ src/screens/main-screen/main-screen.tsx
@@ -1,8 +1,10 @@
 import { FC } from 'react'
 import { Text, View } from 'react-native'
+import Animated, { SlideInDown, SlideInUp } from 'react-native-reanimated'
 
 import { MainLayout } from '@app/shared/layouts/main-layout'
 import { createStyles } from '@app/shared/theme'
+import { ActionButton } from '@app/shared/ui-kit'
 
 export const MainScreen: FC = () => {
   const styles = useStyles()
@@ -10,7 +12,13 @@ export const MainScreen: FC = () => {
   return (
     <MainLayout>
       <View style={styles.root}>
-        <Text>Main screen</Text>
+        <Animated.View entering={SlideInUp.springify()}>
+          <Text style={{ height: 80 }}>Main screen</Text>
+        </Animated.View>
+
+        <Animated.View entering={SlideInDown.springify()}>
+          <ActionButton>Button</ActionButton>
+        </Animated.View>
       </View>
     </MainLayout>
   )
@@ -19,5 +27,6 @@ export const MainScreen: FC = () => {
 const useStyles = createStyles(() => ({
   root: {
     flex: 1,
+    justifyContent: 'space-between',
   },
 }))
