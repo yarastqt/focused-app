066807963efe0759700e221149c9a075d03540fa
yarastqt <yarastqt@gmail.com>
2023-05-07 21:33:35 +0400

    feat: use full height for main layout

--- src/shared/layouts/main-layout/main-layout.tsx
+++ src/shared/layouts/main-layout/main-layout.tsx
@@ -1,7 +1,7 @@
 import { FC, ReactNode } from 'react'
 import { SafeAreaView, StatusBar } from 'react-native'
 
-import { useTheme } from '@app/shared/theme'
+import { createStyles, useTheme } from '@app/shared/theme'
 
 export interface MainLayoutProps {
   children: ReactNode
@@ -11,11 +11,18 @@ export const MainLayout: FC<MainLayoutProps> = (props) => {
   const { children } = props
 
   const { colorScheme } = useTheme()
+  const styles = useStyles()
 
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.root}>
       <StatusBar barStyle={colorScheme === 'dark' ? 'light-content' : 'dark-content'} />
       {children}
     </SafeAreaView>
   )
 }
+
+const useStyles = createStyles(() => ({
+  root: {
+    flex: 1,
+  },
+}))
