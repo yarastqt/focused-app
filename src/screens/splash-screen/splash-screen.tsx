aa7d07a2f556c1a5ea6190e25c68265388c38bb0
yarastqt <yarastqt@gmail.com>
2023-05-07 21:34:09 +0400

    feat: add logo for splash screen

--- src/screens/splash-screen/splash-screen.tsx
+++ src/screens/splash-screen/splash-screen.tsx
@@ -1,17 +1,30 @@
 import { FC } from 'react'
-import { Text, View } from 'react-native'
+import { View } from 'react-native'
 
 import { MainLayout } from '@app/shared/layouts/main-layout'
+import { createStyles } from '@app/shared/theme'
+
+import { Logo } from './ui/logo'
 
 const _SplashScreen: FC = () => {
+  const styles = useStyles()
+
   return (
     <MainLayout>
-      <View>
-        <Text>Splash screen</Text>
+      <View style={styles.root}>
+        <Logo />
       </View>
     </MainLayout>
   )
 }
+
+const useStyles = createStyles(() => ({
+  root: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+}))
 
 export const SplashScreen = Object.assign(_SplashScreen, {
   path: 'splash',
