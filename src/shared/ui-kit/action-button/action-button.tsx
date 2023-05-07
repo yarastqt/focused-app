d1c9acfbd3783ba45459085751256745f2300ad8
yarastqt <yarastqt@gmail.com>
2023-05-08 01:11:30 +0400

    feat: impl action button

---
+++ src/shared/ui-kit/action-button/action-button.tsx
@@ -0,0 +1,70 @@
+import { FC, ReactNode, useCallback } from 'react'
+import { Pressable, Text } from 'react-native'
+import Animated, {
+  interpolateColor,
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from 'react-native-reanimated'
+
+import { createStyles, useTheme } from '@app/shared/theme'
+
+export interface ActionButtonProps {
+  children: ReactNode
+  onPress: () => void
+}
+
+export const ActionButton: FC<ActionButtonProps> = (props) => {
+  const { children, onPress } = props
+
+  const theme = useTheme()
+  const styles = useStyles()
+
+  const scale = useSharedValue(1)
+  const backgroundColor = useSharedValue(0)
+
+  const onPressIn = useCallback(() => {
+    scale.value = withSpring(0.975)
+    backgroundColor.value = withSpring(1)
+  }, [])
+
+  const onPressOut = useCallback(() => {
+    scale.value = withSpring(1)
+    backgroundColor.value = withSpring(0)
+  }, [])
+
+  const rootStyles = useAnimatedStyle(() => {
+    return {
+      transform: [{ scale: scale.value }],
+      backgroundColor: interpolateColor(
+        backgroundColor.value,
+        [0, 1],
+        [theme.color.brandBgBase, theme.color.brandBgPressed],
+      ),
+    }
+  })
+
+  return (
+    <Pressable onPress={onPress} onPressIn={onPressIn} onPressOut={onPressOut}>
+      <Animated.View style={[styles.root, rootStyles]}>
+        <Text style={styles.text}>{children}</Text>
+      </Animated.View>
+    </Pressable>
+  )
+}
+
+const useStyles = createStyles((theme) => ({
+  root: {
+    height: 56,
+    borderRadius: 20,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 10,
+  },
+
+  text: {
+    ...theme.typography.textL,
+    color: theme.color.brandTextOn,
+  },
+}))
