366624cfee061b27c3f0512bf998c9b2dddecfa7
yarastqt <yarastqt@gmail.com>
2023-05-09 18:29:10 +0400

    feat: impl appearance setting

--- src/screens/settings-screen/settings-screen.tsx
+++ src/screens/settings-screen/settings-screen.tsx
@@ -1,17 +1,20 @@
 import { FC } from 'react'
-import { Text, View } from 'react-native'
 
+import { ThemeSwitcher } from '@app/features/theme-switcher'
+import { Light } from '@app/shared/icons'
 import { MainLayout } from '@app/shared/layouts/main-layout'
 import { ScreenHeader } from '@app/shared/ui-kit'
+
+import { Section } from './ui/section'
 
 export const SettingsScreen: FC = () => {
   return (
     <MainLayout>
-      <View>
-        <ScreenHeader>Settings</ScreenHeader>
+      <ScreenHeader>Settings</ScreenHeader>
 
-        <Text>Settings screen</Text>
-      </View>
+      <Section icon={<Light />} title="Appearance">
+        <ThemeSwitcher />
+      </Section>
     </MainLayout>
   )
 }
--- src/shared/icons/index.ts
+++ src/shared/icons/index.ts
@@ -1,3 +1,4 @@
 export * from './arrow-left'
 export * from './cup'
+export * from './light'
 export * from './settings'
