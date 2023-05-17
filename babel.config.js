befe328daad3d35f12377b9750ee7b2105dc0456
yarastqt <yarastqt@gmail.com>
2023-05-18 03:11:16 +0400

    chore: fix babel alias

--- babel.config.js
+++ babel.config.js
@@ -1,29 +1,14 @@
-const path = require('path')
-const config = require('./tsconfig.json')
-
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     [
       'babel-plugin-module-resolver',
       {
-        alias: getAliases(),
+        alias: {
+          '@app': './src',
+        },
       },
     ],
     'react-native-reanimated/plugin',
   ],
-}
-
-function getAliases() {
-  const { baseUrl, paths } = config.compilerOptions
-
-  return Object.entries(paths).reduce((aliases, alias) => {
-    const key = alias[0].replace('/*', '')
-    const value = path.join(baseUrl, alias[1][0].replace('*', ''))
-
-    return {
-      ...aliases,
-      [key]: value,
-    }
-  }, {})
 }
