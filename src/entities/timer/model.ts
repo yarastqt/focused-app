59b0960019b4599dc28a9e17cc5f341e5ffea6d9
yarastqt <yarastqt@gmail.com>
2023-05-21 17:10:33 +0400

    feat: impl timer logic

--- src/entities/timer/model.ts
+++ src/entities/timer/model.ts
@@ -1,13 +1,33 @@
-import { createEvent, createStore, sample } from 'effector'
+import { createEffect, createEvent, createStore, sample } from 'effector'
 import { persist } from 'effector-storage/rn/async'
+import BackgroundTimer from 'react-native-background-timer'
+import { match } from 'ts-pattern'
 
 interface TimerChangedPayload {
   timer: 'cycles' | 'focus' | 'longRest' | 'shortRest'
   value: number
 }
 
+export enum State {
+  INITIAL,
+  FOCUSED_RUN,
+  FOCUSED_END,
+  SHORT_REST_RUN,
+  SHORT_REST_END,
+  LONG_REST_RUN,
+  LONG_REST_END,
+}
+
+const ONE_MINUTE = 60000
+
 export const $$timer = (() => {
   const timerChanged = createEvent<TimerChangedPayload>()
+  const startPressed = createEvent()
+  const giveUpPressed = createEvent()
+
+  const timerUpdated = createEvent<number>()
+
+  const $state = createStore(State.INITIAL)
 
   const $timers = createStore({
     cycles: 5,
@@ -16,7 +36,18 @@ export const $$timer = (() => {
     shortRest: 5,
   })
 
-  const $timer = createStore(10000)
+  const $timer = createStore(0)
+
+  const startTimerFx = createEffect(() => {
+    BackgroundTimer.runBackgroundTimer(() => {
+      // TODO: Find better solution for run loop.
+      timerUpdated($timer.getState() - 1000)
+    }, 1000)
+  })
+
+  const stopTimerFx = createEffect(() => {
+    BackgroundTimer.stopBackgroundTimer()
+  })
 
   persist({ store: $timers, key: 'timers' })
 
@@ -30,9 +61,41 @@ export const $$timer = (() => {
     target: $timers,
   })
 
+  sample({
+    clock: $state,
+    source: $timers,
+    fn: (timers, state) =>
+      match(state)
+        .with(State.FOCUSED_RUN, () => timers.focus * ONE_MINUTE)
+        .with(State.SHORT_REST_RUN, () => timers.shortRest * ONE_MINUTE)
+        .with(State.LONG_REST_RUN, () => timers.longRest * ONE_MINUTE)
+        .otherwise(() => 0),
+    target: timerUpdated,
+  })
+
+  sample({
+    clock: timerUpdated,
+    target: $timer,
+  })
+
+  sample({
+    clock: startPressed,
+    fn: () => State.FOCUSED_RUN,
+    target: [$state, startTimerFx],
+  })
+
+  sample({
+    clock: giveUpPressed,
+    fn: () => State.INITIAL,
+    target: [$state, stopTimerFx],
+  })
+
   return {
+    $state,
     $timer,
     $timers,
     timerChanged,
+    startPressed,
+    giveUpPressed,
   }
 })()
--- src/screens/main-screen/main-screen.tsx
+++ src/screens/main-screen/main-screen.tsx
@@ -1,21 +1,32 @@
+import { useUnit } from 'effector-react'
 import { FC } from 'react'
 import { View } from 'react-native'
+import { match } from 'ts-pattern'
 
+import { $$timer, State } from '@app/entities/timer'
 import { MainLayout } from '@app/shared/layouts/main-layout'
 import { createStyles } from '@app/shared/theme'
 
 import { Action } from './ui/action'
+import { DisplayTimer } from './ui/display-timer'
 import { Header } from './ui/header'
 import { Preview } from './ui/preview'
 
 export const MainScreen: FC = () => {
+  const state = useUnit($$timer.$state)
   const styles = useStyles()
 
   return (
     <MainLayout>
       <View style={styles.root}>
         <Header />
-        <Preview />
+
+        {match(state)
+          .with(State.INITIAL, () => <Preview />)
+          .otherwise(() => (
+            <DisplayTimer />
+          ))}
+
         <Action />
       </View>
     </MainLayout>
