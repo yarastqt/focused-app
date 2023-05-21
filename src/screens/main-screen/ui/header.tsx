8b1897002b00f7e60bcf27ceb2a011aa71b0cb33
yarastqt <yarastqt@gmail.com>
2023-05-21 17:21:46 +0400

    feat: hide actions for non initial state

--- src/screens/main-screen/ui/header.tsx
+++ src/screens/main-screen/ui/header.tsx
@@ -1,6 +1,7 @@
-import { View } from 'react-native'
-import Animated, { FadeInUp } from 'react-native-reanimated'
+import { useUnit } from 'effector-react'
+import Animated, { FadeInUp, FadeOutUp } from 'react-native-reanimated'
 
+import { $$timer, State } from '@app/entities/timer'
 import { Cup, Settings } from '@app/shared/icons'
 import { Route, useNavigation } from '@app/shared/navigation'
 import { createStyles } from '@app/shared/theme'
@@ -12,19 +13,29 @@ export const Header = () => {
   const navigation = useNavigation()
   const styles = useStyles()
 
+  const state = useUnit($$timer.$state)
+
+  const shouldRenderActions = state === State.INITIAL
+
   return (
     <Animated.View entering={FadeInUp.springify()} style={styles.root}>
       <Logo />
 
-      <View style={styles.actions}>
-        <IconButton onPress={() => navigation.navigate(Route.statistics)}>
-          <Cup />
-        </IconButton>
+      {shouldRenderActions && (
+        <Animated.View
+          entering={FadeInUp.springify()}
+          exiting={FadeOutUp.springify()}
+          style={styles.actions}
+        >
+          <IconButton onPress={() => navigation.navigate(Route.statistics)}>
+            <Cup />
+          </IconButton>
 
-        <IconButton onPress={() => navigation.navigate(Route.settings)}>
-          <Settings />
-        </IconButton>
-      </View>
+          <IconButton onPress={() => navigation.navigate(Route.settings)}>
+            <Settings />
+          </IconButton>
+        </Animated.View>
+      )}
     </Animated.View>
   )
 }
